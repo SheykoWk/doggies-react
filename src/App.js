444d718a855e50b6a8549c487dd2f88deb23fc09
SheykoWk <sheyko.wk@gmail.com>
2022-03-29 21:07:24 -0600

    add refresh button

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ function App() {
 
   // Initial state
   const [image, setImage] = useState('')
-
+  const [value, setValue] = useState(0)
 
   useEffect(() => {
-    getDoggies().then((res) => {
-      console.log(res.data)
-      setImage(res.data.message)
-    })
-  }, [])
+    getDoggies()
+      .then((res) => {
+        console.log(res.data)
+        setImage(res.data.message)
+      })
+      .catch((err) => console.log(err))
+  }, [value])
 
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={image} alt='' />
+        <button onClick={() => setValue(value + 1)}>Refresh</button>
       </header>
     </div>
   );
